b9394b52a9a27dfa8d004adaba9c56e1f4f7b2d3
Vova <vovazbuchkin9@gmail.com>
2023-09-06 00:28:39 +0300

    Update store.js

--- src/redux/store.js
+++
@@ -1,5 +0,0 @@
-import { configureStore  } from '@reduxjs/toolkit';
-import { persistReducer, persistStore } from 'redux-persist';
-import storage from 'redux-persist/lib/storage';
-import rootReducer from './reducer';
-
